aea840b10ed029d15831065c0492778dedcc4990
avisiv <avisivayam@gmail.com>
2022-01-14 14:03:08 -0600

    Removed unused functions for minting

--- exchange_booth_skeleton/program/src/processor.rs
+++ exchange_booth_skeleton/program/src/processor.rs
@@ -6,7 +6,6 @@ use solana_program::{
 
 use spl_token::{
     solana_program::program_pack::Pack,
-    state::{Account as TokenAccount, Mint},
 };
 
 use crate::instruction::ExchangeBoothInstruction;
@@ -20,34 +19,6 @@ pub mod withdraw;
 pub struct Processor {}
 
 impl Processor {
-
-    /// Unpacks a spl_token `Account`.
-    pub fn unpack_token_account(
-        account_info: &AccountInfo,
-        token_program_id: &Pubkey,
-    ) -> Result<TokenAccount, ProgramError> {
-        if account_info.owner != token_program_id {
-            return Err(ProgramError::IncorrectProgramId)
-        } else {
-            return TokenAccount::unpack(&account_info.data.borrow())
-                .map_err(|_| ProgramError::InvalidAccountData)
-        }
-    }
-
-    /// Unpacks a spl_token `Mint`.
-    pub fn unpack_mint(
-        account_info: &AccountInfo,
-        token_program_id: &Pubkey,
-    ) -> Result<Mint, ProgramError> {
-        if account_info.owner != token_program_id {
-            Err(ProgramError::IncorrectProgramId)
-        } else {
-            Mint::unpack(&account_info.data.borrow())
-                .map_err(|_| ProgramError::InvalidAccountData)
-        }
-    }
-
-
     pub fn process_instruction(
         program_id: &Pubkey,
         accounts: &[AccountInfo],
