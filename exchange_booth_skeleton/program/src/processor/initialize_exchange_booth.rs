8ead28920ddf0730d0566edb092f3a94e3aa3dfc
Cole Thomson <Cole@Coles-MacBook-Pro.local>
2022-01-14 14:38:16 -0600

    init exchange

--- exchange_booth_skeleton/program/src/processor/initialize_exchange_booth.rs
+++ exchange_booth_skeleton/program/src/processor/initialize_exchange_booth.rs
@@ -1,7 +1,17 @@
 use solana_program::{
+    system_instruction::create_account,
+    system_instruction,
+    sysvar::{rent::Rent, Sysvar},
     account_info::{AccountInfo, next_account_info},
+    program_pack::{Pack},
     entrypoint::ProgramResult, msg, 
+    program_error::ProgramError,
     pubkey::Pubkey,
+    program::{invoke_signed, invoke},
+};
+
+use spl_token::{
+    id, instruction, state::Account
 };
 
 use crate::{
@@ -11,11 +21,120 @@ use crate::{
 
 use borsh::{BorshDeserialize, BorshSerialize};
 
+pub fn assert_with_msg(statement: bool, err: ProgramError, msg: &str) -> ProgramResult {
+    if !statement {
+        msg!(msg);
+        Err(err)
+    } else {
+        Ok(())
+    }
+}
 
 pub fn process(
     program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    accounts: &[AccountInfo]
     // ???
 ) -> ProgramResult {
+    msg!("Instruction: InititializeExchangeBooth");
+    let accounts_iter = &mut accounts.iter();
+
+    let vault_size: usize = Account::LEN;
+    // get accounts
+    let administrator_ai = next_account_info(accounts_iter)?;
+    let exchange_booth_ai = next_account_info(accounts_iter)?;
+    let mint_a_ai = next_account_info(accounts_iter)?;
+    let mint_b_ai = next_account_info(accounts_iter)?;
+    let vault_a_ai = next_account_info(accounts_iter)?;
+    let vault_b_ai = next_account_info(accounts_iter)?;
+    let oracle_ai = next_account_info(accounts_iter)?;
+    let system_program_ai = next_account_info(accounts_iter)?;
+    let token_program_ai = next_account_info(accounts_iter)?;
+    
+    assert_with_msg(
+        administrator_ai.is_signer,
+        ProgramError::MissingRequiredSignature,
+        "first account passed 'administrator' is not a signer as is required.",
+    )?;
+
+    // get PDAs of vaults
+    let(vault_a_key, vault_a_bump) = Pubkey::find_program_address(
+        &[
+            b"vault_a",
+            exchange_booth_ai.key.as_ref()  
+        ],
+        program_id    
+    );
+    
+    msg!("vault a pda: '{:?}', bump: {}", vault_a_key, vault_a_bump);
+
+    assert_with_msg(
+        vault_a_key == *vault_a_ai.key,
+        ProgramError::InvalidArgument,
+        "Key returned from find_program_address (while creating PDA) was not equal to the key passed as the 'vault_a' Account.",
+    )?;
+
+    let(vault_b_key, vault_b_bump) = Pubkey::find_program_address(
+        &[
+            b"vault_b",
+            exchange_booth_ai.key.as_ref()  
+        ],
+        program_id    
+    );
+
+    msg!("vault b pda: '{:?}', bump: {}", vault_b_key, vault_b_bump);
+
+    assert_with_msg(
+        vault_b_key == *vault_b_ai.key,
+        ProgramError::InvalidArgument,
+        "Key returned from find_program_address (while creating PDA) was not equal to the key passed as the 'vault_b' Account.",
+    )?;
+
+    let ix_create_vault_a = system_instruction::create_account(
+       administrator_ai.key,
+       vault_a_ai.key,
+       Rent::get()?.minimum_balance(vault_size),
+       vault_size as u64,
+       token_program_ai.key
+    );
+
+    let ix_create_vault_b = system_instruction::create_account(
+        administrator_ai.key,
+        vault_b_ai.key,
+        Rent::get()?.minimum_balance(vault_size),
+        vault_size as u64,
+        token_program_ai.key
+     );
+
+     // need all accts passed into instruction
+     // signers = same keys that went into pda
+     invoke_signed(&ix_create_vault_a,
+        &[administrator_ai.clone(), vault_a_ai.clone(), token_program_ai.clone(), system_program_ai.clone()], 
+        &[&[b"vault_a", exchange_booth_ai.key.as_ref(), &[vault_a_bump]]])?;
+
+    invoke_signed(&ix_create_vault_a,
+        &[administrator_ai.clone(), vault_b_ai.clone(), token_program_ai.clone(), system_program_ai.clone()], 
+        &[&[b"vault_b", exchange_booth_ai.key.as_ref(), &[vault_b_bump]]])?;
+
+     spl_token::instruction::initialize_account(
+         token_program_ai.key, 
+         &vault_a_key, 
+         mint_a_ai.key, 
+         &vault_a_key);
+
+    spl_token::instruction::initialize_account(
+        token_program_ai.key, 
+        &vault_a_key, 
+        mint_a_ai.key, 
+        &vault_a_key);
+
+     let mut exchange_booth = ExchangeBooth::load_unchecked(exchange_booth_ai)?;
+                exchange_booth.admin = *administrator_ai.key;
+                exchange_booth.mint_a = *mint_a_ai.key;
+                exchange_booth.mint_b = *mint_b_ai.key;
+                exchange_booth.vault_a = *vault_a_ai.key;
+                exchange_booth.vault_b = *vault_b_ai.key;
+                exchange_booth.oracle = *oracle_ai.key;
+                exchange_booth.save(exchange_booth_ai)?;
+
     Ok(())
 }
--- exchange_booth_skeleton/program/src/state.rs
+++ exchange_booth_skeleton/program/src/state.rs
@@ -1,5 +1,8 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::pubkey::Pubkey;
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
+    pubkey::Pubkey,
+};
 use std::mem::size_of;
 
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
@@ -11,5 +14,26 @@ pub struct ExchangeBooth {
     pub vault_a: Pubkey, // token account controlled by PDA
     pub vault_b: Pubkey, // token account controlled by PDA
     pub oracle: Pubkey,
-    // pub spread_bps: u64,
+    // pub spread_bps: u64, // not using right away
+}
+
+impl ExchangeBooth {
+    pub fn load_unchecked(ai: &AccountInfo) -> Result<Self, ProgramError> {
+        Ok(Self::try_from_slice(&ai.data.borrow())?)
+    }
+
+    fn validate(&self) -> ProgramResult {
+        // none
+        Ok(())
+    }
+
+    pub fn load(ai: &AccountInfo) -> Result<Self, ProgramError> {
+        let account = Self::try_from_slice(&ai.data.borrow())?;
+        account.validate()?;
+        Ok(account)
+    }
+
+    pub fn save(&self, ai: &AccountInfo) -> ProgramResult {
+        Ok(self.serialize(&mut *ai.data.borrow_mut())?)
+    }
 }
